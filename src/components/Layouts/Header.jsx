d7efabbe67a5e27ad343eebdd57ed6492f3f0a68
mahmud-alam <official.mahmud23@gmail.com>
2025-02-17 16:34:39 +0600

    navbar design structure

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,17 +1,32 @@
 import { Container, Nav, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import "../../styles/HeaderStyle.css";
+import Logo from "../../assets/logo/logo.png"
 
 function Header() {
   return (
     <>
     <Navbar collapseOnSelect expand="lg">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand href="#home">
+          <Link>
+          <img src={Logo} alt="Logo" className="img-fluid"/>
+          </Link>
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Nav className="ms-auto">
+            <Nav.Link as={Link} to="/">Home</Nav.Link>
+            <Nav.Link as={Link} to="/about">About</Nav.Link>
+            <Nav.Link as={Link} to="/menu">Our Menu</Nav.Link>
+            <Nav.Link as={Link} to="/shop">Shop</Nav.Link>
+            <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
+            <Nav.Link as={Link} to="/cart">
+              <div className="cart">
+                <i class="bi bi-bag fs-5"></i>
+                <em className="roundpoint">2</em>
+              </div>
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
