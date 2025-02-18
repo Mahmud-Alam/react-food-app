a88524d0cbf0753a21bacd7d8b78380082c07791
mahmud-alam <official.mahmud23@gmail.com>
2025-02-18 12:32:34 +0600

    create About Section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Layout from "../../components/Layouts/Layout"
 import HeroSection from "./HeroSection"
+import AboutSection from "./AboutSection"
 import "../../styles/HomeStyle.css"
 
 function Home() {
   return (
     <Layout>
       <HeroSection/>
+      <AboutSection/>
     </Layout>
   )
 }
