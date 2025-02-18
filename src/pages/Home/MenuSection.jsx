8678b322043d0f1ed1ef2e19001dc321697e7056
Mahmud-Alam <official.mahmud23@gmail.com>
2025-02-19 00:29:58 +0600

    add Card component into Menu section of Home page

--- src/pages/Home/MenuSection.jsx
+++ src/pages/Home/MenuSection.jsx
@@ -1,16 +1,112 @@
-import { Col, Container, Row } from "react-bootstrap"
+import { Col, Container, Row } from "react-bootstrap";
+import Card from "../../components/Card.jsx";
+import Image1 from "../../assets/menu/burger-11.jpg";
+import Image2 from "../../assets/menu/burger-12.jpg";
+import Image3 from "../../assets/menu/burger-13.jpg";
+import Image4 from "../../assets/menu/burger-14.jpg";
+import Image5 from "../../assets/menu/burger-15.jpg";
+import Image6 from "../../assets/menu/burger-16.jpg";
+import Image7 from "../../assets/menu/burger-17.jpg";
+import Image8 from "../../assets/menu/burger-18.jpg";
+
+const burgerData = [
+  {
+    id: "0001",
+    image: Image1,
+    title: "Crispy Chicken",
+    description: "Chicken breast, chilli sauce, tomatoes, pickles, coleslaw",
+    rating: 5,
+    price: 99.15,
+  },
+  {
+    id: "0002",
+    image: Image2,
+    title: "Ultimate Bacon",
+    description: "House patty, cheddar cheese, bacon, onion, mustard",
+    rating: 4.5,
+    price: 99.32,
+  },
+  {
+    id: "0003",
+    image: Image3,
+    title: "Black Sheep",
+    description: "American cheese, tomato relish, avocado, lettuce, red onion",
+    rating: 4,
+    price: 69.15,
+  },
+  {
+    id: "0004",
+    image: Image4,
+    title: "Vegan Burger",
+    description: "House patty, cheddar cheese, bacon, onion, mustard",
+    rating: 3.5,
+    price: 99.25,
+  },
+  {
+    id: "0005",
+    image: Image5,
+    title: "Double Burger",
+    description: "2 patties, cheddar cheese, mustard, pickles, tomatoes",
+    rating: 3.0,
+    price: 59.25,
+  },
+  {
+    id: "0006",
+    image: Image6,
+    title: "Turkey Burger",
+    description: "Turkey, cheddar cheese, onion, lettuce, tomatoes, pickles",
+    rating: 3,
+    price: 79.18,
+  },
+  {
+    id: "0007",
+    image: Image7,
+    title: "Smokey House",
+    description: "patty, cheddar cheese, onion, lettuce, tomatoes, pickles",
+    rating: 2.5,
+    price: 99.19,
+  },
+  {
+    id: "0008",
+    image: Image8,
+    title: "Classic Burger",
+    description: "cheddar cheese, ketchup, mustard, pickles, onion",
+    rating: 2.0,
+    price: 89.12,
+  },
+];
 
 function MenuSection() {
   return (
     <section className="menu_section">
-        <Container>
-            <Row>
-                <Col lg={{span:8, offset:2}}>
-                </Col>
-            </Row>
-        </Container>
+      <Container>
+        <Row>
+          <Col lg={{ span: 8, offset: 2 }}>
+            <h2>Our Crazy Burgers</h2>
+            <p>
+              Get ready for a wild ride of flavors! Our crazy burgers are loaded
+              with juicy patties, bold toppings, and irresistible sauces, all
+              stacked on a perfectly toasted bun. Whether you like it spicy,
+              cheesy, or extra meaty, we’ve got a burger that will blow your
+              mind!
+            </p>
+          </Col>
+        </Row>
+        <Row>
+          {burgerData.map((cardData, index) => (
+            <Card 
+            key={index} 
+            image={cardData.image}
+            title={cardData.title}
+            description={cardData.description}
+            rating={cardData.rating}
+            price={cardData.price}
+            />
+          ))}
+        </Row>
+      </Container>
     </section>
-  )
+  );
 }
 
-export default MenuSection
+export default MenuSection;
