4e0be84ce54f22107bb8990565f983a25b55089d
Mahmud-Alam <official.mahmud23@gmail.com>
2025-02-24 19:51:07 +0600

    create Shop Section into Home Page

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,9 +1,10 @@
 import Layout from "../../components/Layouts/Layout"
 import HeroSection from "./HeroSection"
 import AboutSection from "./AboutSection"
-import "../../styles/HomeStyle.css"
 import MenuSection from "./MenuSection"
 import PromotionSection from "./PromotionSection"
+import ShopSection from "./ShopSection"
+import "../../styles/HomeStyle.css"
 
 function Home() {
   return (
@@ -12,6 +13,7 @@ function Home() {
       <AboutSection/>
       <MenuSection/>
       <PromotionSection/>
+      <ShopSection/>
     </Layout>
   )
 }
---
+++ src/pages/Home/ShopSection.jsx
@@ -0,0 +1,36 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import IOS from "../../assets/shop/appstore.png";
+import Android from "../../assets/shop/googleplay.png";
+import DownloadImage from "../../assets/shop/e-shop.png";
+
+function ShopSection() {
+  return (
+    <>
+      <section className="shop_section">
+        <Container>
+          <Row className="align-items-center">
+            <Col lg={6} className="text-center text-lg-start mb-5 mb-lg-0">
+              <h4>Download mobile App and</h4>
+              <h2>save up to 20%</h2>
+              <p>
+                Enjoy your favorite burgers anytime, anywhere! Download our mobile app today and get exclusive deals, easy ordering, and a special 20% discount on your first purchase. Deliciousness is just a tap away!
+              </p>
+              <Link to="">
+              <img src={IOS} alt="ios" className="image-fluid me-3"/>
+              </Link>
+              <Link to="">
+              <img src={Android} alt="android" className="image-fluid me-3"/>
+              </Link>
+            </Col>
+            <Col lg={6}>
+            <img src={DownloadImage} alt="e-shop" className="image-fluid"/>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
+}
+
+export default ShopSection;
