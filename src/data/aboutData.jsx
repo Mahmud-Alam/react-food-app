15e7157299bf71605c99312ff3cbf27a8139f6ee
Mahmud-Alam <official.mahmud23@gmail.com>
2025-02-26 23:35:40 +0600

    separate About section and data

---
+++ src/data/aboutData.jsx
@@ -0,0 +1,23 @@
+import Pizza from "../assets/about/pizza.png";
+import Salad from "../assets/about/salad.png";
+import Delivery from "../assets/about/delivery-bike.png";
+
+const aboutData = [
+  {
+    image: Pizza,
+    title: "Original",
+    description: `Crafted with the finest ingredients, our pizzas are a perfect blend of crispy crust, rich sauce, and gooey, melted cheese. Whether you love classic flavors or adventurous toppings, every slice delivers pure satisfaction`,
+  },
+  {
+    image: Salad,
+    title: "Qualty Foods",
+    description: `Fresh, vibrant, and packed with nutrients—our salads are a refreshing mix of crisp greens, juicy veggies, and flavorful dressings. Whether as a light meal or a tasty side, each bowl is a burst of goodness`,
+  },
+  {
+    image: Delivery,
+    title: "Fastest Delivery",
+    description: `Craving something delicious but don’t want to step out? Our fast and reliable delivery service ensures your favorite meals arrive hot and fresh at your doorstep. Just sit back, relax, and enjoy!`,
+  },
+];
+
+export default aboutData;
