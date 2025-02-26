a32cb219e51cab6f62bd1255af25dbce7a7dc69a
Mahmud-Alam <official.mahmud23@gmail.com>
2025-02-26 23:28:45 +0600

    get review data, display into review section component

---
+++ src/data/reviewData.jsx
@@ -0,0 +1,41 @@
+import img1 from "../assets/blog/review-author-1.jpg";
+import img2 from "../assets/blog/review-author-2.jpg";
+import img3 from "../assets/blog/review-author-3.jpg";
+import img4 from "../assets/blog/review-author-4.jpg";
+
+const reviewData = [
+  {
+    id: 1,
+    name: "Ruhul Amin",
+    image: img1,
+    review:
+      "The best burger I’ve ever had! The flavors were perfect, and the crispy onions added the perfect crunch. Will definitely be coming back for more!",
+    rating: 5,
+  },
+  {
+    id: 2,
+    name: "Shahnaz Begum",
+    image: img2,
+    review:
+      "I’m obsessed with the quality of the ingredients here. Everything is so fresh, and the burger is cooked to perfection. Highly recommend!",
+    rating: 5,
+  },
+  {
+    id: 3,
+    name: "Abdur Rahman",
+    image: img3,
+    review:
+      "Absolutely delicious! I tried the spicy burger, and it was packed with flavor. The delivery was fast, and the burger was still hot when it arrived. A must-try!",
+    rating: 4.5,
+  },
+  {
+    id: 4,
+    name: "Fariha Khan",
+    image: img4,
+    review:
+      "Amazing food and great service! I love how every bite feels like a burst of flavor. The mobile app made ordering super easy, and the 20% discount was a great bonus!",
+    rating: 4,
+  },
+];
+
+export default reviewData;
