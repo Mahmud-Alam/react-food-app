51d2fd3fcde8b3013df140fa49839ae229183fbb
Mahmud-Alam <official.mahmud23@gmail.com>
2025-02-28 00:39:50 +0600

    complete Contact section component and css design

--- src/components/home/ContactSection.jsx
+++ src/components/home/ContactSection.jsx
@@ -1,7 +1,23 @@
+import { Col, Container, Row } from "react-bootstrap";
+import { Link } from "react-router-dom";
+
 function ContactSection() {
   return (
-    <section className="contact_section"></section>
-  )
+    <section className="contact_section">
+      <Container>
+        <Row className="justify-content-center">
+          <Col sm={8} className="text-center">
+            <h4>We Guarantee</h4>
+            <h2>30 Minutes Delivery!</h2>
+            <p>
+              Craving something delicious? We guarantee your meal will be at your door in 30 minutes or less!
+            </p>
+            <Link to="/" className="btn btn_red px-4 rounded-0">Order Now</Link>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  );
 }
 
-export default ContactSection
+export default ContactSection;
